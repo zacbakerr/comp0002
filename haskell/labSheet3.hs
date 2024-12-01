fc22bd227acb00a760b223bcdc72b8d9ab4e661e
zacbakerr <zacattackbaker@gmail.com>
2024-12-01 17:41:50 +0000

    finished lab sheet 3

--- haskell/labSheet3.hs
+++ haskell/labSheet3.hs
@@ -2,24 +2,33 @@ isEven :: Int -> Bool
 isEven num = num `mod` 2 == 0
 
 
-mult :: [Int] -> Int
+mult :: Num a => [a] -> a
 mult [] = 0
 mult list = foldr (*) 1 list
 
-posList :: [Int] -> [Int]
+posList :: (Num a, Ord a) => [a] -> [a]
 posList [] = []
 posList list = filter (> 0) list
 
 trueList :: [Bool] -> Bool
-trueList list = foldr (==) True list
+trueList list = foldr (&&) True list
 
 evenList :: [Int] -> Bool
-evenList list = foldr (==) True (map isEven list)
+evenList list = trueList (map (isEven) list)
 
 -- maxList :: [Int] -> Int
 
-inRange :: Int -> Int -> [Int] -> [Int]
+inRange :: Ord a => a -> a -> [a] -> [a]
 inRange s e list = filter (<= e) (filter (>= s) list)
 
-countPositives :: [Int] -> Int
+countPositives :: (Num a, Ord a) => [a] -> Int
 countPositives list = length(filter (> 0) list)
+
+myLength :: String -> Int
+myLength str = foldr (+) 0 (map (\_ -> 1) str)
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap func list = foldr (\x acc -> func x : acc) [] list
+
+myLength' :: String -> Int
+myLength' str = foldr (+) 0 (foldr (\x acc -> (\_ -> 1) x : acc) [] str)
